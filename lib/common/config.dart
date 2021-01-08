27cb6b407d62e5df4dff9d24f8ed02102f1cfccf
Doan Tan Trung <dttrung.ityu@gmail.com>
2021-01-08 21:07:18 +0700

    init comit

---
+++ lib/common/config.dart
@@ -0,0 +1,12 @@
+export 'config/index.dart';
+
+const serverConfig = {
+  "type": "woo",
+  "url": "https://plussizeclothingshop.com",
+  "consumerKey": "ck_628623aee807645143354f770662c8947ea63f86",
+  "consumerSecret": "cs_477e91d9e9658e2445ab957d6b5f593ad6784007",
+  "blog":
+      "https://mstore.io", //Your website woocommerce. You can remove this line if it same url
+  /// remove to use as native screen
+  "forgetPassword": "https://mstore.io/wp-login.php?action=lostpassword"
+};
