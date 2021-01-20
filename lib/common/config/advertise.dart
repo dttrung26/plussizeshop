39adf730b130e1a4c99094811a2485b7f5938a45
jonu2528 <30666344+jonu2528@users.noreply.github.com>
2021-01-20 15:49:18 +0700

    big size clothing shop

--- lib/common/config/advertise.dart
+++ lib/common/config/advertise.dart
@@ -12,7 +12,9 @@ enum kAdType {
 
 const kAdConfig = {
   "enable": true,
-  "isGoogleBannerEnable": false,
+  "isGoogleBannerEnable": true,
+
+  ///false,
   //do not set "type": kAdType.googleBanner.
   "type": kAdType.googleInterstitial,
 
@@ -31,10 +33,10 @@ const kAdConfig = {
   "androidUnitBanner": "ca-app-pub-2101182411274198/4052745095",
   "androidUnitInterstitial": "ca-app-pub-2101182411274198/7131168728",
   "androidUnitReward": "ca-app-pub-2101182411274198/6939597036",
-  "iosAppId": "ca-app-pub-2101182411274198~6923444927",
-  "iosUnitBanner": "ca-app-pub-2101182411274198/5418791562",
-  "iosUnitInterstitial": "ca-app-pub-2101182411274198/9218413691",
-  "iosUnitReward": "ca-app-pub-2101182411274198/9026842008",
+  "iosAppId": "ca-app-pub-4607450770813571~5435708636",
+  "iosUnitBanner": "ca-app-pub-4607450770813571/9760491675",
+  "iosUnitInterstitial": "ca-app-pub-4607450770813571/6942756648",
+  "iosUnitReward": "a-app-pub-4607450770813571/9377348292",
   "waitingTimeToDisplayInterstitial": 10,
   "waitingTimeToDisplayReward": 10,
 };
